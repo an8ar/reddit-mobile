24e5eed6f9c5a3023a39d00abbacdec4c0acdb03
Ansar <annssarr@gmail.com>
2024-07-09 16:38:14 +0500

    add: native to the project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import "react-native-reanimated";
+
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 const Home = () => {
   return (
     <SafeAreaView>
-      <Text>Homess</Text>
+      <Text className="text-3xl">Homessss</Text>
     </SafeAreaView>
   );
 };

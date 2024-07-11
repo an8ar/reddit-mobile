0ecab0252ce03d79b8a280f0e6fc310581ea3331
Ansar <annssarr@gmail.com>
2024-07-11 13:49:09 +0500

    refactor: left drawer

---
+++ src/shared/ui/left-drawer.tsx
@@ -0,0 +1,57 @@
+import { Drawer } from 'expo-router/drawer';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+import { useNavigation } from '@react-navigation/native';
+import { DrawerActions } from '@react-navigation/native';
+import {
+  DrawerContentComponentProps,
+  DrawerContentScrollView,
+  DrawerItem,
+} from '@react-navigation/drawer';
+
+interface LeftDrawerProps {
+  toggleRightDrawer: () => void;
+}
+
+export const LeftDrawerScreen = ({ toggleRightDrawer }: LeftDrawerProps) => {
+  const navigation = useNavigation();
+
+  return (
+    <Drawer
+      screenOptions={{
+        drawerIcon: ({ size, focused }) => {
+          return <Ionicons name="menu-sharp" size={24} color="black" />;
+        },
+        headerShown: true,
+        headerTitle: 'toggle',
+        drawerStyle: { width: '80%' },
+        drawerPosition: 'left',
+        headerLeft(props) {
+          return (
+            <Ionicons
+              name="menu-sharp"
+              size={24}
+              color="black"
+              onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+            />
+          );
+        },
+        headerRight(props) {
+          return <FontAwesome name="reddit" size={24} color="black" onPress={toggleRightDrawer} />;
+        },
+        headerRightContainerStyle: { paddingRight: 12 },
+        headerLeftContainerStyle: { paddingLeft: 12 },
+      }}
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+    />
+  );
+};
+function CustomDrawerContent(props: DrawerContentComponentProps) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItem label="Home" onPress={() => props.navigation.navigate('communities')} />
+      <DrawerItem label="Settings" onPress={() => props.navigation.navigate('chat')} />
+    </DrawerContentScrollView>
+  );
+}

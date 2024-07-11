d5eb5a2c42217d0975070df42a9eec312d1b13fe
Ansar <annssarr@gmail.com>
2024-07-11 13:20:22 +0500

    add: multiple drawers with tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import 'react-native-reanimated';
+import { Drawer as DrawerLayout } from 'react-native-drawer-layout';
 import { Drawer } from 'expo-router/drawer';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import {
@@ -14,7 +15,6 @@ import { DrawerActions } from '@react-navigation/native';
 
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { View, Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -33,35 +33,9 @@ export default function RootLayout() {
   if (!loaded) {
     return null;
   }
-  const navigation = useNavigation();
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer
-        screenOptions={{
-          drawerIcon: ({ size, focused }) => {
-            return <Ionicons name="menu-sharp" size={24} color="black" />;
-          },
-          headerShown: true,
-          headerTitle: 'togle',
-          drawerStyle: { width: '80%' },
-          headerLeft(props) {
-            return (
-              <Ionicons
-                name="menu-sharp"
-                size={24}
-                color="black"
-                onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
-              />
-            );
-          },
-          headerRight(props) {
-            return <Ionicons name="menu-sharp" size={24} color="black" />;
-          },
-          headerRightContainerStyle: { paddingRight: 12 },
-          headerLeftContainerStyle: { paddingLeft: 12 },
-        }}
-        drawerContent={(props) => <CustomDrawerContent {...props} />}
-      />
+      <RightDrawerScreen />
     </GestureHandlerRootView>
   );
 }
@@ -69,11 +43,70 @@ export default function RootLayout() {
 function CustomDrawerContent(props: DrawerContentComponentProps) {
   return (
     <DrawerContentScrollView {...props}>
-      <DrawerItem
-        label="Home sadfkdsjfkdsjfksdjfkdjfkdj"
-        onPress={() => props.navigation.navigate('communities')}
-      />
+      <DrawerItem label="Home" onPress={() => props.navigation.navigate('communities')} />
       <DrawerItem label="Settings" onPress={() => props.navigation.navigate('chat')} />
     </DrawerContentScrollView>
+  );
+}
+
+interface LeftDrawerProps {
+  toggleRightDrawer: () => void;
+}
+
+const LeftDrawerScreen = ({ toggleRightDrawer }: LeftDrawerProps) => {
+  const navigation = useNavigation();
+
+  return (
+    <Drawer
+      screenOptions={{
+        drawerIcon: ({ size, focused }) => {
+          return <Ionicons name="menu-sharp" size={24} color="black" />;
+        },
+        headerShown: true,
+        headerTitle: 'togle',
+        drawerStyle: { width: '80%' },
+        drawerPosition: 'left',
+        headerLeft(props) {
+          return (
+            <Ionicons
+              name="menu-sharp"
+              size={24}
+              color="black"
+              onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+            />
+          );
+        },
+        headerRight(props) {
+          return <Ionicons name="menu-sharp" size={24} color="black" onPress={toggleRightDrawer} />;
+        },
+        headerRightContainerStyle: { paddingRight: 12 },
+        headerLeftContainerStyle: { paddingLeft: 12 },
+      }}
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+    />
+  );
+};
+
+function RightDrawerScreen() {
+  const [rightDrawerOpen, setRightDrawerOpen] = React.useState(false);
+  const toggleDrawer = () => {
+    setRightDrawerOpen((prev) => !prev);
+  };
+  return (
+    <DrawerLayout
+      open={rightDrawerOpen}
+      onOpen={() => setRightDrawerOpen(true)}
+      onClose={() => setRightDrawerOpen(false)}
+      drawerPosition="right"
+      renderDrawerContent={() => (
+        <>
+          <View>
+            <Text>wwws</Text>
+          </View>
+        </>
+      )}
+    >
+      <LeftDrawerScreen toggleRightDrawer={toggleDrawer} />
+    </DrawerLayout>
   );
 }

8bab1874ee6a0d49acaf1f1c6546ec6c258739e1
Ansar <annssarr@gmail.com>
2024-07-12 12:23:21 +0500

    add: native wind global style

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import 'react-native-reanimated';
+import { NativeWindStyleSheet } from 'nativewind';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import App from '~/app-root';
@@ -10,6 +11,9 @@ import App from '~/app-root';
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+NativeWindStyleSheet.setOutput({
+  default: 'native',
+});
 export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),

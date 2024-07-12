31ea1e29ccefaca171061eec887df1d6d9235fba
Ansar <annssarr@gmail.com>
2024-07-12 12:23:46 +0500

    add: user drawer content

--- src/entities/user/ui/user-drawer-content.tsx
+++ src/entities/user/ui/user-drawer-content.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { DrawerItem } from '@react-navigation/drawer';
 import { View, Text } from 'react-native';
 
 import { UserDrawerLayout } from './user-drawer-layout';
@@ -11,14 +13,19 @@ interface Props {
 export const UserDrawerContent = ({ handleClose }: Props) => {
   return (
     <UserDrawerLayout handleClose={handleClose}>
-      <View className="gap-10">
-        <Text>UserDrawerContent</Text>
-        <Text>UserDrawerContent</Text>
-
-        <Text>UserDrawerContent</Text>
-
-        <Text>UserDrawerContent</Text>
+      <View className="items-center">
+        <FontAwesome5 name="user-circle" size={20 * 4} color="#9ca3af" />
       </View>
+      <View className="my-5 border-b border-gray-200 pb-2">
+        <Text className="text-center text-lg font-light">
+          Sign up to upvote the best content, customize you feed, share your interests, and more!
+        </Text>
+      </View>
+      <DrawerItem
+        label="Sign-up / Login"
+        onPress={() => console.log('ww')}
+        icon={() => <FontAwesome5 name="user-circle" size={24} color="#1c1917" />}
+      />
     </UserDrawerLayout>
   );
 };

60a38946a52f76e2c24ed2450f04083d4606ff62
Ansar <annssarr@gmail.com>
2024-07-15 17:11:50 +0500

    add: login widget

---
+++ src/widgets/auth/ui/login-widget.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+import { AuthHeader, LoginForm, TermsAndConditions } from '~/entities/auth';
+
+export const LoginWidget = () => {
+  return (
+    <View className="p-2">
+      <AuthHeader />
+      <View className="mt-10">
+        <Text className="text-center text-2xl font-semibold">Log in</Text>
+        <TermsAndConditions />
+        <LoginForm />
+      </View>
+    </View>
+  );
+};

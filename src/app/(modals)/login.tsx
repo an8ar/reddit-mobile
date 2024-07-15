a9ad0604cd52747b83683e2c7cc7298f90c898db
Ansar <annssarr@gmail.com>
2024-07-15 17:11:50 +0500

    add: login widget

--- src/app/(modals)/login.tsx
+++ src/app/(modals)/login.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
 import { useRouter } from 'expo-router';
+import { ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { AuthHeader } from '~/entities/auth';
+import { LoginWidget } from '~/widgets/auth';
 
 const Login = () => {
   const router = useRouter();
 
   return (
     <SafeAreaView>
-      <AuthHeader />
+      <ScrollView>
+        <LoginWidget />
+      </ScrollView>
     </SafeAreaView>
   );
 };

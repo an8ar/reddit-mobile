639c318cd5c4a3cbbae45363baf35995a15abad4
Ansar <annssarr@gmail.com>
2024-07-15 11:45:55 +0500

    add: screens for drawers and login modal

--- src/app-root/index.tsx
+++ src/app-root/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
+import { Stack } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
-
-import { RightDrawerScreen } from '~/shared';
 
 import { store } from './store';
 
@@ -11,7 +10,11 @@ const App = () => {
   return (
     <Provider store={store}>
       <SafeAreaProvider>
-        <RightDrawerScreen />
+        <Stack initialRouteName="(modals)/login">
+          <Stack.Screen name="(modals)/login" options={{ presentation: 'modal' }} />
+
+          <Stack.Screen name="(drawers)" options={{ headerShown: false }} />
+        </Stack>
       </SafeAreaProvider>
     </Provider>
   );

3244ad2fc925321e8da0f8008a66e431dcff6756
Ansar <annssarr@gmail.com>
2024-07-17 16:52:07 +0500

    add: create password widget

--- src/widgets/auth/index.ts
+++ src/widgets/auth/index.ts
@@ -2,3 +2,4 @@ export * from './ui/login-widget';
 export * from './ui/sign-up/create-email-widget';
 export * from './ui/sign-up/verify-email-widget';
 export * from './ui/sign-up/create-username-widget';
+export * from './ui/sign-up/create-password-widget';

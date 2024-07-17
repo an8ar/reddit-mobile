997a982aa9bbb1d0619eb7867d812f5b93137d50
Ansar <annssarr@gmail.com>
2024-07-17 13:15:31 +0500

    refactor: wrapped with view

--- src/entities/auth/ui/terms-and-conditions.tsx
+++ src/entities/auth/ui/terms-and-conditions.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 export const TermsAndConditions = () => {
   return (
-    <Text className="mt-8 px-4 text-center font-light text-gray-500">
-      By continuing, you agree to our{' '}
-      <Text className="font-medium text-black underline">User Agreement</Text> and acknowledge that
-      you understand the <Text className="font-medium text-black underline">Privacy Policy</Text>
-    </Text>
+    <View>
+      <Text className="mt-8 px-4 text-center font-light text-gray-500">
+        By continuing, you agree to our{' '}
+        <Text className="font-medium text-black underline">User Agreement</Text> and acknowledge
+        that you understand the{' '}
+        <Text className="font-medium text-black underline">Privacy Policy</Text>
+      </Text>
+    </View>
   );
 };

4449a712faf79741e5d5718278f647479deb3a40
Ansar <annssarr@gmail.com>
2024-07-17 16:52:28 +0500

    add: create password page

--- src/pages/auth/index.ts
+++ src/pages/auth/index.ts
@@ -2,3 +2,4 @@ export * from './login-page';
 export * from './sign-up/create-email-page';
 export * from './sign-up/verify-email-page';
 export * from './sign-up/create-username-page';
+export * from './sign-up/create-password-page';

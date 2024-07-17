f59b4645bd542ab65894d9ddcd2cdeb6767a3f03
Ansar <annssarr@gmail.com>
2024-07-17 16:39:03 +0500

    add: create username widget

--- src/widgets/auth/index.ts
+++ src/widgets/auth/index.ts
@@ -1,3 +1,4 @@
 export * from './ui/login-widget';
 export * from './ui/sign-up/create-email-widget';
 export * from './ui/sign-up/verify-email-widget';
+export * from './ui/sign-up/create-username-widget';

eeb32a6bb28767c90947c7cd6534935e3abe54b3
Ansar <annssarr@gmail.com>
2024-07-17 12:08:48 +0500

    add: sign up widget/screen

--- src/app/(modals)/sign-up.tsx
+++ src/app/(modals)/sign-up.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { KeyboardAvoidingView, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { SignUpWidget } from '~/widgets/auth';
 
 const SignUp = () => {
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView className="flex-1">
+      <ScrollView>
+        <KeyboardAvoidingView behavior="position" keyboardVerticalOffset={50}>
+          <SignUpWidget />
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
--- src/widgets/auth/index.ts
+++ src/widgets/auth/index.ts
@@ -1,1 +1,2 @@
 export * from './ui/login-widget';
+export * from './ui/sign-up-widget';

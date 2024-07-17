24629023d7b402ea17eac8fc3dd5b50e2dc4f0a5
Ansar <annssarr@gmail.com>
2024-07-17 12:08:11 +0500

    add: navigation to sign-up to auth header

--- src/entities/auth/ui/auth-header.tsx
+++ src/entities/auth/ui/auth-header.tsx
@@ -4,12 +4,24 @@ import { AntDesign } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
 import { View, TouchableOpacity, Image, Text } from 'react-native';
 
-export const AuthHeader = () => {
+interface Props {
+  isLogin: boolean;
+}
+
+export const AuthHeader = ({ isLogin }: Props) => {
   const router = useRouter();
+
   const handleBack = () => {
-    router.back();
+    router.replace('(drawers)/(tabs)');
   };
 
+  const handleLogin = () => {
+    router.push('(modals)/login');
+  };
+
+  const handleSignUp = () => {
+    router.push('(modals)/sign-up');
+  };
   return (
     <View className="flex-row items-center justify-between">
       <View>
@@ -17,17 +29,22 @@ export const AuthHeader = () => {
           <AntDesign name="close" size={32} color="#94a3b8" />
         </TouchableOpacity>
       </View>
-
       <View className="relative h-10 w-10">
         <Image
           source={require('../../../../assets/images/reddit.png')}
           className="absolute left-1/2 top-0 h-10 w-10 -translate-x-1/2"
         />
       </View>
-
-      <View>
-        <Text className="text-lg font-semibold text-gray-500">Sign up</Text>
-      </View>
+      {isLogin && (
+        <TouchableOpacity onPress={handleSignUp}>
+          <Text className="text-lg font-semibold text-gray-500">Sign up</Text>
+        </TouchableOpacity>
+      )}
+      {!isLogin && (
+        <TouchableOpacity onPress={handleLogin}>
+          <Text className="text-lg font-semibold text-gray-500">Login</Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
 };

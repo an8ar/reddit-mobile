35a1358e96491355e3c696830eff74d58216fb7f
Ansar <annssarr@gmail.com>
2024-07-17 15:51:01 +0500

    add: go back header for sign up form steps

--- src/entities/auth/index.ts
+++ src/entities/auth/index.ts
@@ -2,3 +2,4 @@ export * from './ui/auth-header';
 export * from './ui/terms-and-conditions';
 export * from './ui/login-form';
 export * from './ui/sign-up-form/create-email-form';
+export * from './ui/go-back-header';

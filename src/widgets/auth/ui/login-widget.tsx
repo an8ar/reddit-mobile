11e4fcee8e2deac2d1ddb8a8bac6e4ea4e2565d0
Ansar <annssarr@gmail.com>
2024-07-17 12:08:11 +0500

    add: navigation to sign-up to auth header

--- src/widgets/auth/ui/login-widget.tsx
+++ src/widgets/auth/ui/login-widget.tsx
@@ -7,7 +7,7 @@ import { AuthHeader, LoginForm, TermsAndConditions } from '~/entities/auth';
 export const LoginWidget = () => {
   return (
     <View className="p-2">
-      <AuthHeader />
+      <AuthHeader isLogin={true} />
       <View className="mt-10">
         <Text className="text-center text-2xl font-semibold">Log in</Text>
         <TermsAndConditions />

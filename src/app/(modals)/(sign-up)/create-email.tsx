dc0ae5bc4a9b60dadcf96d86aed7e8426ce65085
Ansar <annssarr@gmail.com>
2024-07-17 14:46:00 +0500

    add: create email widget

---
+++ src/app/(modals)/(sign-up)/create-email.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { KeyboardAvoidingView, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { CreateEmailWidget } from '~/widgets/auth';
+
+const CreateEmail = () => {
+  return (
+    <SafeAreaView className="flex-1">
+      <ScrollView>
+        <KeyboardAvoidingView behavior="position" keyboardVerticalOffset={50}>
+          <CreateEmailWidget />
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default CreateEmail;

f729720bc71746ce35fce3b36b8664b05324cbc2
Ansar <annssarr@gmail.com>
2024-07-17 16:39:03 +0500

    add: create username widget

---
+++ src/widgets/auth/ui/sign-up/create-username-widget.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+import { CreateUsernameForm, GoBackHeader } from '~/entities/auth';
+
+export const CreateUsernameWidget = () => {
+  return (
+    <View className="p-2">
+      <GoBackHeader hasSkip />
+      <View className="mt-10">
+        <View>
+          <Text className="text-center text-2xl font-semibold">Create your username</Text>
+          <Text className="text-center text-lg">Most redditors use an anonymous username.</Text>
+          <Text className="text-center text-lg">You won't be able to change it later.</Text>
+        </View>
+        <CreateUsernameForm />
+      </View>
+    </View>
+  );
+};

9c160de855af3a2874551bc26b4109a809320fde
Ansar <annssarr@gmail.com>
2024-07-19 15:41:19 +0500

    add: post interactions

---
+++ src/entities/post/ui/post-interactions.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { View } from 'react-native';
+
+import { PostVote } from './post-vote';
+
+export const PostInteractions = () => {
+  return (
+    <View className="flex-row">
+      <PostVote />
+    </View>
+  );
+};

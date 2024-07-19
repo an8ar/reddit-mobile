7c40cd36a7363b58c75c2c68833cc71f77b68327
Ansar <annssarr@gmail.com>
2024-07-19 15:41:28 +0500

    add: post card

--- src/entities/post/ui/post-card.tsx
+++ src/entities/post/ui/post-card.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import Entypo from '@expo/vector-icons/Entypo';
+import { View, Text, TouchableWithoutFeedback } from 'react-native';
 
-const PostCard = () => {
+export const PostCard = () => {
+  const onPress = () => {
+    console.log('options pressed');
+  };
+
   return (
-    <View>
-      <Text>PostCard</Text>
+    <View className="mb-2">
+      <View className="flex-row items-center justify-between">
+        <View className="flex-row items-center gap-2">
+          <View className="h-5 w-5 rounded-full bg-black" />
+          <Text>an8ar</Text>
+          <Text className="text-xs text-gray-600">9h</Text>
+        </View>
+        <TouchableWithoutFeedback onPress={onPress}>
+          <View>
+            <Entypo name="dots-three-horizontal" size={16} color="black" />
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+
+      <View>
+        <Text className="text-lg font-semibold">Frontend Reddit</Text>
+        <Text className="text-xs font-light text-gray-800">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit dolorem perspiciatis
+          asperiores cupiditate veritatis. Excepturi voluptate totam beatae vel eligendi officiis
+          soluta sint, aut eos, ab dolorum maiores debitis qui.
+        </Text>
+      </View>
     </View>
   );
 };
-
-export default PostCard;
